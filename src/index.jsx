4e8947f305ce8133f9fb01d0ba73cec8954d8ffe
Seth Brouhard <seth.j.brouhard@gmail.com>
2026-05-21 10:44:23 -0600

    Covert to vite

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,6 @@ import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 
@@ -33,8 +32,3 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
